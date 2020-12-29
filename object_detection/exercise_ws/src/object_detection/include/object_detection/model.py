219ae42caf9090e440d618611e134ba996c91879
Himanshu Arora <himanshuaror@gmail.com>
2020-12-29 16:08:18 -0500

    Initialize model and load weights

--- object_detection/exercise_ws/src/object_detection/include/object_detection/model.py
+++ object_detection/exercise_ws/src/object_detection/include/object_detection/model.py
@@ -1,14 +1,27 @@
+import torch
+from .yolov5.models.yolo import Model
 
 
 class NoGPUAvailable(Exception):
     pass
 
-class Wrapper():
-    def __init__(self, model_file):
-        # TODO Instantiate your model and other class instances here!
-        # TODO Don't forget to set your model in evaluation/testing/production mode, and sending it to the GPU
-        # TODO If no GPU is available, raise the NoGPUAvailable exception
-        pass
+
+class Wrapper:
+    """
+    Loads a pre-trained YOLOv5 model for inference
+    """
+    def __init__(self, model_file, image_size=224):
+        self.weights_path = model_file
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"    # YOLOv5-s should work fine on CPU as well
+        self.model = Model("/code/exercise_ws/src/object_detection/include/object_detection/yolov5/models/yolov5s.yaml", ch=3, nc=2)
+        weights = torch.load(self.weights_path, map_location=self.device)
+        print(self.model.load_state_dict(weights))
+        self.model.eval()
+        self.classes = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        self.class_id = tuple(range(len(self.classes)))
+        self.im_sz = image_size
+        print(f"Loaded model {self.weights_path} to detect {self.classes} with ids {self.class_id}")
+
     def predict(self, batch_or_image):
         # TODO Make your model predict here!
 
@@ -33,9 +46,3 @@ class Wrapper():
             scores.append(score)
 
         return boxes, labels, scores
-
-class Model():    # TODO probably extend a TF or Pytorch class!
-    def __init__(self):
-        # TODO Instantiate your weights etc here!
-        pass
-    # TODO add your own functions if need be!
