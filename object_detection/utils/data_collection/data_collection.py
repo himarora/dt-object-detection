436c2a906e52d1b3e12cabb13af1b27a77db407d
Himanshu Arora <himanshuaror@gmail.com>
2020-12-28 19:18:44 -0500

    Update data collection for sim

--- object_detection/utils/data_collection/data_collection.py
+++ object_detection/utils/data_collection/data_collection.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import numpy as np
-
+import os
 from agent import PurePursuitPolicy
 from utils import launch_env, seed
+import cv2
 from utils import launch_env, seed, makedirs, display_seg_mask, display_img_seg_mask
 
-DATASET_DIR="../../dataset"
+DATASET_DIR="/home/himarora/PycharmProjects/dt-exercises-object-det/object_detection/dataset/sim_data/images"
 
 npz_index = 0
 def save_npz(img, boxes, classes):
@@ -15,11 +16,25 @@ def save_npz(img, boxes, classes):
         np.savez(f"{DATASET_DIR}/{npz_index}.npz", *(img, boxes, classes))
         npz_index += 1
 
+
+def get_bbox_classes(seg_img):
+    pass
+
+
+def save_image(img, seg=False):
+    global npz_index
+    dir_name = "true" if not seg else "seg"
+    dir_path = f"{DATASET_DIR}/{dir_name}"
+    os.makedirs(dir_path, exist_ok=True)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f"{dir_path}/{npz_index}.png", img)
+    npz_index += 1 if seg else 0
+
+
 def clean_segmented_image(seg_img):
-    # TODO
     # Tip: use either of the two display functions found in util.py to ensure that your cleaning produces clean masks
     # (ie masks akin to the ones from PennFudanPed) before extracting the bounding boxes
-    pass
+    return seg_img
     # return boxes, classes
 
 seed(123)
@@ -45,8 +60,11 @@ while True:
         rewards.append(rew)
         environment.render(segment=int(nb_of_steps / 50) % 2 == 0)
 
-        # TODO boxes, classes = clean_segmented_image(segmented_obs)
-        # TODO save_npz(obs, boxes, classes)
+        image = clean_segmented_image(segmented_obs)
+        save_image(obs, seg=False)
+        save_image(image, seg=True)
+        # boxes, classes = clean_segmented_image(segmented_obs)
+        # save_npz(obs, boxes, classes)
 
         nb_of_steps += 1
 
